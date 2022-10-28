8db7d7def2eea054da44326ff3927de39f594954
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-10-28 16:04:38 +0700

    create router & react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./routes/root";
+import { CategoryRoute } from "./routes/category";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BookmarkRoute } from "./routes/bookmark";
+import "./configs/axiosConfig";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "category/:categoryId",
+    element: <CategoryRoute />,
+  },
+  {
+    path: "bookmark",
+    element: <BookmarkRoute />,
+  },
+]);
+
+export const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
